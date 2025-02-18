42561d8c10cd22cf4662e0c317f157cd674ac50f
juan <up240214@alumnos.upa.edu.mx>
2025-02-18 09:50:17 -0600

    tercer dia

---
+++ test.py
@@ -0,0 +1,31 @@
+#Program 1
+#Declare your age as integer variable
+print("Ingresa tu edad: ")
+edad=18
+print(type(edad))
+print(int(edad))
+# Program 2 
+#Declare your height as a float variable
+print("Ingresa tu estatura")
+Heigth= 1.65
+print(type(Heigth))
+print(float(Heigth))
+#Program 3
+#Declare a variable that store a complex number
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex numbers: ',(1 + 1j) * (1 - 1j))
+
+
+
+
+#Program 4 
+#Write a scrip that prompts the yuser to enter base and height
+# of the trangle and calculate an area of this triangle 
+# (area= 0.5 x b x h)
+base= float(input("ingresa el valor de la base: "))
+altura= float(input("ingresa el valor de la altura: "))
+area= (0.5 * base * altura)
+print("El area del triangulo es: " + str(area))
+
+#Program 5
+#whrite a crop that promots to enter side a, side 
