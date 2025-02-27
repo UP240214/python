adb585953a8d2cdc8a6fb9099db48a895f1f4959
juan <up240214@alumnos.upa.edu.mx>
2025-02-27 11:53:18 -0600

    avance dia 4

--- 04_DayExercises.py
+++ 04_DayExercises.py
@@ -1,1 +1,25 @@
-print("hola mundo")
+#Program 1
+print("")
+print("EJERCISE 1:")
+word1= "thirty"
+word2= " days"
+word3= " of"
+word4=" python"
+completeSentence= word1 + word2 +word3 +word4 
+print(completeSentence)
+print("")
+###############################################################################
+#program 3
+
+print("EJERCISE 2:")
+fir1="Coding"
+sec1=" for "
+ter1="all"
+com1= fir1 + sec1 + ter1
+print(com1)
+print("")
+################################################################################
+#Program 4
+com1= "company"
+print("company")
+
