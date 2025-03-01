f44bbca6133ad38ed43ba37f7258234f77418d3d
juan <up240214@alumnos.upa.edu.mx>
2025-02-28 23:27:51 -0600

    avance en casa

--- 04_DayExercises.py
+++ 04_DayExercises.py
@@ -9,7 +9,7 @@ completeSentence= word1 + word2 +word3 +word4
 print(completeSentence)
 print("")
 ###############################################################################
-#program 3
+#program 2
 
 print("EJERCISE 2:")
 fir1="Coding"
@@ -19,7 +19,28 @@ com1= fir1 + sec1 + ter1
 print(com1)
 print("")
 ################################################################################
+#Program 3
+print("EJERCICIO 3: ")
+company= com1
+print("")
+################################################################################
 #Program 4
-com1= "company"
-print("company")
+print("EJERCICIO 4: ")
+print(company)
+print("")
+################################################################################
+#Program 5
+word5= "company"
+dist= len(word5)
+print(f"Length of '{word5}':", dist)
+print("")
+#################################################################################
+#Program 6
+word5= "company"
+minn= upper(word5)
+print(f"Uppercase of '{word5}':", minn)
+print("")
+
+
+
 
