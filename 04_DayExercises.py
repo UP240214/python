5a4015bf00eea39324e0b7917eada43cf1d45daf
juan <up240214@alumnos.upa.edu.mx>
2025-03-04 09:36:46 -0600

    avancote en clase

--- 04_DayExercises.py
+++ 04_DayExercises.py
@@ -39,23 +39,100 @@ print("")
 word5= "company"
 print("Texto en mayúsculas:", word5.upper())
 print("")
-#################################################################################
+##################################################################################
 #Program 7
 word5= "company"
 print("Texto en mayúsculas:", word5.lower())
 print("")
-#################################################################################
+##################################################################################
 #Program 8
 sentence= "Coding for all"
 print("capitalize():", sentence.capitalize())  # Primera letra en mayúscula
 print("title():", sentence.title())  # Primera letra de cada palabra en mayúscula
 print("swapcase():", sentence.swapcase())  # Invierte mayúsculas y minúsculas
 print("")
-#################################################################################
+##################################################################################
 #Program 9
 sentence= "Coding for all"
 print("Sin la primera palabra:", sentence[sentence.find(" ") + 1:])
+print("") 
+##################################################################################
+#Program 10
+subcadena=sentence
+print(subcadena.find("codificacion"))
+print("")
+##################################################################################
+#Program 11
+sentence11= "for all"
+word11= "Coding "
+print(word11 + sentence11)
+print("")
+##################################################################################
+#Program 12
+word12= "Python for Everyone "
+wrdreplace="all"
+newword= word12.replace("Everyone", "all")
+print(newword)
+print("")
+##################################################################################
+#Program 13
+word13 = "Coding For All"
+newword13 = word13.split(" ")
+print(newword13)
+print("")
+##################################################################################
+#Program 14
+word14= "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" 
+newword14 = word14.split(" ")
+print(newword14)
+print("")
+##################################################################################
+#Program 15
+word15 = "Coding For All"
+newword15= word15[0]
+print(newword15)
 print("")
 #################################################################################
-#Program 10
+#Program 16
+word16 = "Coding For All"
+newword16 = len(word16) -1
+print(newword16)
+print(word16[-1])
+print("")
+#################################################################################
+#Program 17
+word17 = "Coding For All"
+newword17 = word17[10]
+print(newword17)
+print("")
+##################################################################################
+#Program 18
+word18= "Python For Everyone"
+PPP=word18[0]
+FFF=word18[7]
+EEE=word18[11]
+print(PPP,FFF,EEE)
+print("")
+##################################################################################
+#Program 19
+word19= "Coding For All"
+w191=word19[0]
+w192=word19[7]
+w193=word19[11]
+print(w191,w192,w193)
+print("")
+##################################################################################
+#Program 20
+word20 = "Coding For All"
+print(word20.find("C"))
+print("")
+##################################################################################
+#Program 21
+word20 = "Coding For All"
+print(word20.find("F"))
+print("")
+##################################################################################
+
+
+
 
